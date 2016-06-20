af2e640f3f3d79956f8038fcc83087ebda3e64a6
卢渊 <yuanlua@isoftstone.com>
2016-06-20 16:02:45 +0800

    [Featrue]完成webview加载h5界面和进度条梳理

---
+++ app/src/main/java/com/tianfeng/webviewpro/MainActivity.java
@@ -0,0 +1,43 @@
+package com.tianfeng.webviewpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mWebView = (WebView) this.findViewById(R.id.wv_webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);//让webview支持js
+        mWebView.loadUrl("http://www.baidu.com");
+        mWebView.setWebViewClient(new MyWebViewClient());
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK && mWebView.canGoBack()){
+            mWebView.canGoBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
